3c5734b9a5ab489ba4f526a49963681bbf6fc178
Calvin <calvinlin6@gmail.com>
2022-12-24 21:36:55 -0500

    commit tree 09339d57616b69281fe3465694a52d1dc034c9ca

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    //todo
+    return string.toLowerCase();
+}
+
+function logShout (aWord) {
+    console.log(aWord.toUpperCase( ));
+  }
+
+  function logWhisper (aWord) {
+    console.log(aWord.toLowerCase( ));
+  }
+
+  function whisper(string) {
+    //todo
+    return string.toLowerCase();
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    var cantAnswer = "I can't hear you!";
+    if (string.toLowerCase(string) === string) 
+        return cantAnswer;
+    }
